f016f691a9324a6be892448b58acbd813c1befd7
lucian <lghinet@totalsoft.ro>
2025-04-01 13:43:47 +0300

    Fix subscribe max concurrent messages (#57)
    * fix subscribe max concurrent messages
    
    * minor upgrades
    
    * fix race condition on tests

--- pkg/messaging/jetstream/jetstream_test.go
+++ pkg/messaging/jetstream/jetstream_test.go
@@ -1,12 +1,21 @@
 package jetstream
 
 import (
+	"context"
 	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/jetstream"
 	"reflect"
+	"rusi/pkg/messaging"
+	"sync"
 	"testing"
+	"time"
 )
 
-func Test_parseMetadata(t *testing.T) {
+func Ptr[T any](v T) *T {
+	return &v
+}
+
+func Test_jetStreamPubSub_parseMetadata(t *testing.T) {
 	tests := []struct {
 		name       string
 		properties map[string]string
@@ -50,5 +59,174 @@ func Test_parseMetadata(t *testing.T) {
 				t.Errorf("parseNATSStreamingMetadata() got = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+func Test_jetStreamPubSub_Subscribe_serial_messages(t *testing.T) {
+	srv := RunBasicJetStreamServer()
+	defer shutdownJSServerAndRemoveStorage(t, srv)
+	natsConn, err := nats.Connect(srv.ClientURL())
+	if err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	//defer cancel()
+	defer natsConn.Close()
+
+	js, err := jetstream.New(natsConn)
+	if err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+	_, _ = js.CreateStream(ctx, jetstream.StreamConfig{Name: "foo", Subjects: []string{"test.*"}})
+
+	sut := &jetStreamPubSub{
+		options: options{
+			durableSubscriptionName: "consumerID",
+			ackWaitTime:             time.Second,
+			maxInFlight:             1,
+		},
+		natsConn: natsConn,
+		closed:   false,
+		ctx:      ctx,
+		cancel:   cancel,
+	}
+
+	subject := "test.topic"
+	_ = sut.Publish(subject, &messaging.MessageEnvelope{Payload: "test1"})
+	_ = sut.Publish(subject, &messaging.MessageEnvelope{Payload: "test2"})
+
+	done := false
+	var mu sync.Mutex
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	unsub, err := sut.Subscribe(subject, func(ctx context.Context, msg *messaging.MessageEnvelope) error {
+		switch msg.Payload {
+		case "test1":
+			time.Sleep(time.Millisecond * 100)
+			mu.Lock()
+			done = true
+			mu.Unlock()
+		case "test2":
+			mu.Lock()
+			if !done {
+				t.Errorf("Message1 not processed yet")
+			}
+			mu.Unlock()
+		default:
+			t.Errorf("Unexpected message payload: %s", msg.Payload)
+		}
+		wg.Done()
+		return nil
+	}, nil)
+
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+	doneCh := make(chan struct{})
+	go func() { wg.Wait(); close(doneCh) }()
+	select {
+	case <-doneCh:
+	case <-ctx.Done():
+		t.Errorf("timeout waiting for message processing")
+		return
+	}
+
+	err = unsub()
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	err = sut.Close()
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+}
+
+func Test_jetStreamPubSub_Subscribe_two_concurrent_messages(t *testing.T) {
+	srv := RunBasicJetStreamServer()
+	defer shutdownJSServerAndRemoveStorage(t, srv)
+	natsConn, err := nats.Connect(srv.ClientURL())
+	if err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	//defer cancel()
+	defer natsConn.Close()
+
+	js, err := jetstream.New(natsConn)
+	if err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+	_, _ = js.CreateStream(ctx, jetstream.StreamConfig{Name: "foo", Subjects: []string{"test.*"}})
+
+	sut := &jetStreamPubSub{
+		options: options{
+			durableSubscriptionName: "consumerID",
+			ackWaitTime:             time.Second,
+			maxInFlight:             1,
+		},
+		natsConn: natsConn,
+		closed:   false,
+		ctx:      ctx,
+		cancel:   cancel,
+	}
+
+	subject := "test.topic"
+	_ = sut.Publish(subject, &messaging.MessageEnvelope{Payload: "test1"})
+	_ = sut.Publish(subject, &messaging.MessageEnvelope{Payload: "test2"})
+	_ = sut.Publish(subject, &messaging.MessageEnvelope{Payload: "test3"})
+
+	done := false
+	wg := &sync.WaitGroup{}
+	wg.Add(3)
+	var mu sync.Mutex
+	unsub, err := sut.Subscribe(subject, func(ctx context.Context, msg *messaging.MessageEnvelope) error {
+		switch msg.Payload {
+		case "test1":
+			time.Sleep(time.Millisecond * 100)
+			mu.Lock()
+			done = true
+			mu.Unlock()
+		case "test2":
+			mu.Lock()
+			if done {
+				t.Errorf("Message2 should not wait for message1")
+			}
+			mu.Unlock()
+			time.Sleep(time.Millisecond * 100)
+		case "test3":
+			mu.Lock()
+			if !done {
+				t.Errorf("Message3 has to wait for the other 2 to finish")
+			}
+			mu.Unlock()
+		default:
+			t.Errorf("Unexpected message payload: %s", msg.Payload)
+		}
+		wg.Done()
+		return nil
+	}, &messaging.SubscriptionOptions{MaxConcurrentMessages: Ptr(int32(2))})
+
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	doneCh := make(chan struct{})
+	go func() { wg.Wait(); close(doneCh) }()
+	select {
+	case <-doneCh:
+	case <-ctx.Done():
+		t.Errorf("timeout waiting for message processing")
+		return
+	}
+
+	err = unsub()
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	err = sut.Close()
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
 	}
 }
